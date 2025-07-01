da7fc4360513d45811176a58854ec1e963b96e0e
maria <fesychmarimia@gmail.com>
2025-07-01 12:15:23 +0200

    fix contactById

---
+++ src/routers/contacts.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import {
+  getAllContactsController,
+  getContactByIdController,
+} from '../controllers/contacts.js';
+const router = Router();
+
+router.get('/contacts', getAllContactsController);
+
+router.get('/contacts/:contactId', getContactByIdController);
+
+export default router;
